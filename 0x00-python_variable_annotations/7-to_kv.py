eeac12b14556af0e4becbbc17cf196fb19d46d3d
koechMR <briankipkoech328@gmail.com>
2024-01-11 19:12:28 +0300

    sum

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-'''Task 7's module.
+''' rep module
 '''
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    '''Converts a key and its value to a tuple of the key and
-    the square of its value.
+    ''' key and its value
     '''
     return (k, float(v**2))
