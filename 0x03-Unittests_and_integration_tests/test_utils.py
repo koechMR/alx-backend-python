08cb2f20bb418ed11a1507f35faa704ee142bd30
koechMR <briankipkoech328@gmail.com>
2024-02-05 10:04:46 +0300

    test cases

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+""" Module for testing utils """
+
+from parameterized import parameterized
+import unittest
+import requests
+from unittest.mock import patch
+from utils import (access_nested_map, get_json, memoize)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Class for Testing """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ Test that the method """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Test that a KeyError"""
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
+
+
+class TestGetJson(unittest.TestCase):
+    """ Testing Get Json """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ utils.get_json returns the expected"""
+        config = {'return_value.json.return_value': test_payload}
+        patcher = patch('requests.get', **config)
+        mock = patcher.start()
+        self.assertEqual(get_json(test_url), test_payload)
+        mock.assert_called_once()
+        patcher.stop()
+
+
+class TestMemoize(unittest.TestCase):
+    """ Testing Memoize """
+
+    def test_memoize(self):
+        """ Test that when calling a_property twice
+        """
+
+        class TestClass:
+            """ Test Class for wrapping with memoize """
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
